efa1ae06bf9a581681022d09e1d5249ede3296fa
Nathan Toups <n@rjrbt.io>
2025-02-25 22:08:04 -0700

    fix: remove npm build step

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,6 @@ DIST_ID=$RJRBT_CDN_DIST_ID
 CACHE_ID=$(date +%s)
 FORMATTED_JSON='{"Paths": {"Quantity": 1,"Items": ["/*"]},"CallerReference":"'${CACHE_ID}'"}'
 
-npm run build
 cd $DIR
 aws s3 sync . $BUCKET --acl public-read
 aws cloudfront create-invalidation --distribution-id "$DIST_ID" --invalidation-batch "$FORMATTED_JSON"
