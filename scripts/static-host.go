5fa50b3e9a197ec59b8db88435565aeff62b98f7
Nathan Toups <n@rjrbt.io>
2025-02-28 19:30:00 +0100

    wip

--- scripts/static-host.go
+++ scripts/static-host.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./src")))
+	dirPath := flag.String("dir", "./src", "directory path to use")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*dirPath)))
 	http.ListenAndServe(":3000", nil)
 }
